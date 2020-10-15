9dce4ca1091b236606f40e48fc3a6b2342ea4abd
BoppanaSandeep <boppanasandeep57@gmail.com>
2020-10-15 21:09:17 +0530

    unkn

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
 import {
-  NavController,
-  ActionSheetController,
-  ToastController,
-  Platform,
-  LoadingController,
-  Loading
+    NavController,
+    ActionSheetController,
+    ToastController,
+    Platform,
+    LoadingController,
+    Loading
 } from "ionic-angular";
 
 import { File } from "@ionic-native/file";
@@ -14,237 +14,237 @@ import { FilePath } from "@ionic-native/file-path";
 import { Camera } from "@ionic-native/camera";
 import { SecondPage } from "../second/second";
 import {
-  NativePageTransitions,
-  NativeTransitionOptions
+    NativePageTransitions,
+    NativeTransitionOptions
 } from "@ionic-native/native-page-transitions";
 import * as nodemailer from "nodemailer";
 
 declare var cordova: any;
 
 @Component({
-  selector: "page-home",
-  templateUrl: "home.html"
+    selector: "page-home",
+    templateUrl: "home.html"
 })
 export class HomePage {
-  lastImage: string = null;
-  loading: Loading;
+    lastImage: string = null;
+    loading: Loading;
 
-  constructor(
-    public navCtrl: NavController,
-    private camera: Camera,
-    private transfer: Transfer,
-    private file: File,
-    private filePath: FilePath,
-    public actionSheetCtrl: ActionSheetController,
-    public toastCtrl: ToastController,
-    public platform: Platform,
-    public loadingCtrl: LoadingController,
-    private nativePageTransitions: NativePageTransitions
-  ) {
+    constructor(
+        public navCtrl: NavController,
+        private camera: Camera,
+        private transfer: Transfer,
+        private file: File,
+        private filePath: FilePath,
+        public actionSheetCtrl: ActionSheetController,
+        public toastCtrl: ToastController,
+        public platform: Platform,
+        public loadingCtrl: LoadingController,
+        private nativePageTransitions: NativePageTransitions
+    ) {
 
-  }
-
-  public secondPageFlip() {
-    let options: NativeTransitionOptions = {
-      direction: "up",
-      duration: 600
-    };
-    this.nativePageTransitions.flip(options);
-    this.navCtrl.setRoot(SecondPage);
-  }
-
-  public secondPageCurl() {
-    let options: NativeTransitionOptions = {
-      direction: "up",
-      duration: 600
-    };
-    this.nativePageTransitions.curl(options);
-    this.navCtrl.setRoot(SecondPage);
-  }
-
-  public secondPageSlide() {
-    let options: NativeTransitionOptions = {
-      direction: "left",
-      duration: 400,
-      slowdownfactor: -1,
-      iosdelay: 50
-    };
-
-    this.nativePageTransitions.slide(options);
-    this.navCtrl.setRoot(SecondPage);
-  }
-
-  public presentActionSheet() {
-    let actionSheet = this.actionSheetCtrl.create({
-      title: "Select Image Sources dsfsdf xvdsfgsdf sdf sdf s sd sdf sfe",
-      buttons: [
-        {
-          text: "Load from Library",
-          handler: () => {
-            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
-          }
-        },
-        {
-          text: "Use Camera",
-          handler: () => {
-            this.takePicture(this.camera.PictureSourceType.CAMERA);
-          }
-        },
-        {
-          text: "Cancel",
-          role: "cancel"
-        }
-      ]
-    });
-    actionSheet.present();
-  }
-
-  public takePicture(sourceType) {
-    // Create options for the Camera Dialog
-    var options = {
-      quality: 100,
-      sourceType: sourceType,
-      saveToPhotoAlbum: false,
-      correctOrientation: true
-    };
-
-    // Get the data of an image
-    this.camera.getPicture(options).then(
-      imagePath => {
-        // Special handling for Android library
-        if (
-          this.platform.is("android") &&
-          sourceType === this.camera.PictureSourceType.PHOTOLIBRARY
-        ) {
-          this.filePath.resolveNativePath(imagePath).then(filePath => {
-            let correctPath = filePath.substr(0, filePath.lastIndexOf("/") + 1);
-            let currentName = imagePath.substring(
-              imagePath.lastIndexOf("/") + 1,
-              imagePath.lastIndexOf("?")
-            );
-            this.copyFileToLocalDir(
-              correctPath,
-              currentName,
-              this.createFileName()
-            );
-          });
-        } else {
-          var currentName = imagePath.substr(imagePath.lastIndexOf("/") + 1);
-          var correctPath = imagePath.substr(0, imagePath.lastIndexOf("/") + 1);
-          this.copyFileToLocalDir(
-            correctPath,
-            currentName,
-            this.createFileName()
-          );
-        }
-      },
-      err => {
-        this.presentToast("Error while selecting image.");
-      }
-    );
-  }
-
-  private createFileName() {
-    var d = new Date(),
-      n = d.getTime(),
-      newFileName = n + ".jpg";
-    return newFileName;
-  }
-
-  // Copy the image to a local folder
-  private copyFileToLocalDir(namePath, currentName, newFileName) {
-    this.file
-      .copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName)
-      .then(
-        success => {
-          this.lastImage = newFileName;
-        },
-        error => {
-          this.presentToast("Error while storing file.");
-        }
-      );
-  }
-
-  private presentToast(text) {
-    let toast = this.toastCtrl.create({
-      message: text,
-      duration: 3000,
-      position: "top"
-    });
-    toast.present();
-  }
-
-  // Always get the accurate path to your apps folder
-  public pathForImage(img) {
-    if (img === null) {
-      return "";
-    } else {
-      return cordova.file.dataDirectory + img;
     }
-  }
 
-  public uploadImage() {
-    // Destination URL
-    var url = "http://localhost/ionic/upload.php";
+    public secondPageFlip() {
+        let options: NativeTransitionOptions = {
+            direction: "up",
+            duration: 600
+        };
+        this.nativePageTransitions.flip(options);
+        this.navCtrl.setRoot(SecondPage);
+    }
 
-    // File for Upload
-    var targetPath = this.pathForImage(this.lastImage);
+    public secondPageCurl() {
+        let options: NativeTransitionOptions = {
+            direction: "up",
+            duration: 600
+        };
+        this.nativePageTransitions.curl(options);
+        this.navCtrl.setRoot(SecondPage);
+    }
 
-    // File name only
-    var filename = this.lastImage;
+    public secondPageSlide() {
+        let options: NativeTransitionOptions = {
+            direction: "left",
+            duration: 400,
+            slowdownfactor: -1,
+            iosdelay: 50
+        };
 
-    var options = {
-      fileKey: "file",
-      fileName: filename,
-      chunkedMode: false,
-      mimeType: "multipart/form-data",
-      params: { fileName: filename }
-    };
+        this.nativePageTransitions.slide(options);
+        this.navCtrl.setRoot(SecondPage);
+    }
 
-    const fileTransfer: TransferObject = this.transfer.create();
+    public presentActionSheet() {
+        let actionSheet = this.actionSheetCtrl.create({
+            title: "Select Image Sources dsfsdf xvdsfgsdf sdf sdf s sd sdf sfe",
+            buttons: [
+                {
+                    text: "Load from Library",
+                    handler: () => {
+                        this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+                    }
+                },
+                {
+                    text: "Use Camera",
+                    handler: () => {
+                        this.takePicture(this.camera.PictureSourceType.CAMERA);
+                    }
+                },
+                {
+                    text: "Cancel",
+                    role: "cancel"
+                }
+            ]
+        });
+        actionSheet.present();
+    }
 
-    this.loading = this.loadingCtrl.create({
-      content: "Uploading..."
-    });
-    this.loading.present();
+    public takePicture(sourceType) {
+        // Create options for the Camera Dialog
+        var options = {
+            quality: 100,
+            sourceType: sourceType,
+            saveToPhotoAlbum: false,
+            correctOrientation: true
+        };
 
-    // Use the FileTransfer to upload the image
-    fileTransfer.upload(targetPath, url, options).then(
-      data => {
-        this.loading.dismissAll();
-        this.presentToast("Image succesful uploaded.");
-      },
-      err => {
-        this.loading.dismissAll();
-        this.presentToast("Error while uploading file.");
-      }
-    );
-  }
+        // Get the data of an image
+        this.camera.getPicture(options).then(
+            imagePath => {
+                // Special handling for Android library
+                if (
+                    this.platform.is("android") &&
+                    sourceType === this.camera.PictureSourceType.PHOTOLIBRARY
+                ) {
+                    this.filePath.resolveNativePath(imagePath).then(filePath => {
+                        let correctPath = filePath.substr(0, filePath.lastIndexOf("/") + 1);
+                        let currentName = imagePath.substring(
+                            imagePath.lastIndexOf("/") + 1,
+                            imagePath.lastIndexOf("?")
+                        );
+                        this.copyFileToLocalDir(
+                            correctPath,
+                            currentName,
+                            this.createFileName()
+                        );
+                    });
+                } else {
+                    var currentName = imagePath.substr(imagePath.lastIndexOf("/") + 1);
+                    var correctPath = imagePath.substr(0, imagePath.lastIndexOf("/") + 1);
+                    this.copyFileToLocalDir(
+                        correctPath,
+                        currentName,
+                        this.createFileName()
+                    );
+                }
+            },
+            err => {
+                this.presentToast("Error while selecting image.");
+            }
+        );
+    }
 
-  sendEmailUsingNodemailer() {
-    debugger;
-    console.log(nodemailer);
-    // var transporter = nodemailer.createTransport({
-    //   service: "gmail",
-    //   auth: {
-    //     user: "eesapplications@gmail.com",
-    //     pass: "ees.applications"
-    //   }
-    // });
+    private createFileName() {
+        var d = new Date(),
+            n = d.getTime(),
+            newFileName = n + ".jpg";
+        return newFileName;
+    }
 
-    // var mailOptions = {
-    //   from: "eesapplications@gmail.com",
-    //   to: "sboppana@ameripro-solutions.com",
-    //   subject: "Sending Email using Node.js",
-    //   html: "<h1>Welcome</h1><p>That was easy!</p>"
-    // };
+    // Copy the image to a local folder
+    private copyFileToLocalDir(namePath, currentName, newFileName) {
+        this.file
+            .copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName)
+            .then(
+                success => {
+                    this.lastImage = newFileName;
+                },
+                error => {
+                    this.presentToast("Error while storing file.");
+                }
+            );
+    }
 
-    // transporter.sendMail(mailOptions, function(error, info) {
-    //   if (error) {
-    //     console.log(error);
-    //   } else {
-    //     console.log("Email sent: " + info.response);
-    //   }
-    // });
-  }
+    private presentToast(text) {
+        let toast = this.toastCtrl.create({
+            message: text,
+            duration: 3000,
+            position: "top"
+        });
+        toast.present();
+    }
+
+    // Always get the accurate path to your apps folder
+    public pathForImage(img) {
+        if (img === null) {
+            return "";
+        } else {
+            return cordova.file.dataDirectory + img;
+        }
+    }
+
+    public uploadImage() {
+        // Destination URL
+        var url = "http://localhost/ionic/upload.php";
+
+        // File for Upload
+        var targetPath = this.pathForImage(this.lastImage);
+
+        // File name only
+        var filename = this.lastImage;
+
+        var options = {
+            fileKey: "file",
+            fileName: filename,
+            chunkedMode: false,
+            mimeType: "multipart/form-data",
+            params: { fileName: filename }
+        };
+
+        const fileTransfer: TransferObject = this.transfer.create();
+
+        this.loading = this.loadingCtrl.create({
+            content: "Uploading..."
+        });
+        this.loading.present();
+
+        // Use the FileTransfer to upload the image
+        fileTransfer.upload(targetPath, url, options).then(
+            data => {
+                this.loading.dismissAll();
+                this.presentToast("Image succesful uploaded.");
+            },
+            err => {
+                this.loading.dismissAll();
+                this.presentToast("Error while uploading file.");
+            }
+        );
+    }
+
+    sendEmailUsingNodemailer() {
+        debugger;
+        console.log(nodemailer);
+        // var transporter = nodemailer.createTransport({
+        //   service: "gmail",
+        //   auth: {
+        //     user: "eesapplications@gmail.com",
+        //     pass: "ees.applications"
+        //   }
+        // });
+
+        // var mailOptions = {
+        //   from: "eesapplications@gmail.com",
+        //   to: "sboppana@ameripro-solutions.com",
+        //   subject: "Sending Email using Node.js",
+        //   html: "<h1>Welcome</h1><p>That was easy!</p>"
+        // };
+
+        // transporter.sendMail(mailOptions, function(error, info) {
+        //   if (error) {
+        //     console.log(error);
+        //   } else {
+        //     console.log("Email sent: " + info.response);
+        //   }
+        // });
+    }
 }
